1edb3ac085b3f10733e20e7f030939898128ab15
frquxl <frquxl@gmail.com>
2025-09-05 17:39:59 +0100

    feat: implement --version flag that reflects git tag

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"os/exec"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,4 +11,17 @@ var RootCmd = &cobra.Command{
 	Use:   "atelier",
 	Short: "A metaphor-driven CLI for software project management",
 	Long:  `Atelier is a CLI tool that uses the atelier/artist/canvas metaphor to help manage software projects.`,
+}
+
+func init() {
+	RootCmd.Version = getVersion()
+}
+
+func getVersion() string {
+	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
+	output, err := cmd.Output()
+	if err != nil {
+		return "dev"
+	}
+	return strings.TrimSpace(string(output))
 }
